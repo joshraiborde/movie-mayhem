d4d715524dcb09ad840a803f9ded457a042178b2
joshraiborde <joshraiborde@gmail.com>
2021-09-26 17:19:09 -0400

    added api for featured and search, added form for search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState} from 'react'
+import Movie from './components/Movie'
 
+// const FEATURED_API = "saved on local machine because i haven't figured out .env and .gitignore yet";
+
+
+
+// const SEARCH_API = "saved on local machine because i haven't figured out .env and .gitignore yet";
+// const SEARCH_API = "saved on local machine because i haven't figured out .env and .gitignore yet"
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    getMovies(FEATURED_API)
+  }, [])
+  
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results)
+        setMovies(data.results);
+      });
+  
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm) {
+      getMovies(SEARCH_API + searchTerm)
+
+      setSearchTerm('');
+    }
+  }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <header>
+      <form onSubmit={handleOnSubmit}>
+      <input
+        className="search"
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleOnChange}
+         />
+      </form>
+    </header>
+    <div className="movie-container">
+    {movies.length > 0 && movies.map(movie=> (
+      <Movie key={movie.id} {...movie} />
+    ))}
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
